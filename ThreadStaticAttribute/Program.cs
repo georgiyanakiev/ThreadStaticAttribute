df983977a3ce03495162ae2d2d56f11fee3a7a69
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2020-01-03 17:19:43 +0000

    changes

--- ThreadStaticAttribute/Program.cs
+++ ThreadStaticAttribute/Program.cs
@@ -7,62 +7,15 @@ using System.Threading;
 
 namespace AdvancedTasks
 {
-    public static class Program
+    public static class Program6
     {
         public static int counter;
-        public static ThreadLocal<int> local =
-            new ThreadLocal<int>(() =>
-              {
-                  return Thread.CurrentThread.ManagedThreadId;
-              });
+        
         public static void Main()
         {
-            new Thread(() =>
-            {
-                for ( int x = 0; x<local.Value; x++)
-                {
-                    counter++;
-                    Console.WriteLine("Thread 1: {0}", x);
-                }
-            }).Start();
 
-            new Thread(() =>
-            {
-                for (int x = 0; x < local.Value; x++)
-                {
-                    counter++;
-                    Console.WriteLine("Thread 2: {0}", x);
-                }
-            }).Start();
-            //bool stop = false;
-            //Thread t = new Thread(new ThreadStart(() =>
-
-            //{
-            //    while (!stop)
-            //    {
-            //        Console.WriteLine("Thread is running...");
-            //        Thread.Sleep(2000);
-            //    }
-
-            //}));
-            ////t.IsBackground = true;
-            //t.Start();
-            //t.Join();
-            Console.WriteLine("Press any key to exit");
+            Console.WriteLine("Press any key to continue!");
             Console.ReadLine();
-
-            //stop = true;
-
-
-            //t.Join();
-
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    Console.WriteLine("This is main Thread!");
-            //    Thread.Sleep(0);
-            //}
-            //t.Join();
-            //Console.ReadLine();
         }
     }
 }
